522566e3c3bea6110b0cdac56009e61e82e45c91
Dale <dludwins@outlook.com>
2023-07-10 23:05:56 -0400

    fixed cols

---
+++ backend/apis/version1/_supabase/route_votes.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from fastapi import APIRouter
+from sqlalchemy.orm import Session
+from fastapi import Depends
---
+++ backend/db/_supabase/connect_to_db.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  2 22:54:46 2023
+
+@author: dale
+"""
+
+from db._supabase import supa_client
+import base64
+
+def get_reviews_list() -> list[dict]:    
+    bucket = supa_client.get_cc_bucket()
+    folder_path = 'reviews'
+    return bucket.list(path=folder_path)
+       
+
+def get_image_from_results(file_path: str):
+    bucket = supa_client.get_cc_bucket()
+    img_bytes = bucket.download(
+        path=file_path
+    )
+    return base64.b64encode(img_bytes).decode()
