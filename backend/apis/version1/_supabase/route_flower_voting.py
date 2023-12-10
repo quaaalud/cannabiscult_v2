9e8ba963bb3c8a34920210e0290ce941ee487447
Dale <dludwins@outlook.com>
2023-12-10 15:26:08 -0500

    switched flower to new db

--- backend/apis/version1/_supabase/route_flower_voting.py
+++ backend/apis/version1/_supabase/route_flower_voting.py
@@ -12,6 +12,7 @@ from sqlalchemy import func
 from fastapi import Depends
 from db.session import get_supa_db
 from db.models.flower_voting import FlowerVoting
+from db.models.flower_rankings import Flower_Ranking
 from db.repository.flower_voting import add_new_flower_vote
 from db.repository.flower_reviews import get_review_data_and_path
 from schemas.flower_voting import FlowerVoteCreate
@@ -90,5 +91,47 @@ async def get_top_strains(db: Session = Depends(get_supa_db)):
             return_strains.append(flower_data)
         except:
             pass
+
+    return return_strains
+
+
+@router.get("/get_top_rated_flower_strains", response_model=list)
+async def get_top_flower_strains(db: Session = Depends(get_supa_db)):
+
+    avg_ratings = (
+        db.query(
+            Flower_Ranking.strain,
+            Flower_Ranking.cultivator,
+            func.avg(Flower_Ranking.appearance_rating),
+            func.avg(Flower_Ranking.smell_rating),
+            func.avg(Flower_Ranking.flavor_rating),
+            func.avg(Flower_Ranking.effects_rating),
+            func.avg(Flower_Ranking.harshness_rating),
+            func.avg(Flower_Ranking.freshness_rating),
+        )
+        .group_by(Flower_Ranking.strain, Flower_Ranking.cultivator)
+        .all()
+    )
+    
+    scored_strains = []
+    for strain in avg_ratings:
+        overall_score = sum(filter(None, strain[2:])) / 6
+        scored_strains.append((strain[0], strain[1], round(overall_score, 2)))
+    scored_strains.sort(key=lambda x: x[2], reverse=True)
+    top_strains = scored_strains[:3]
+    
+    return_strains = []
+    for strain_dict in top_strains:
+        print(strain_dict)
+        flower_data = get_flower_and_description(
+            db,
+            strain=strain_dict[0],
+            cultivar_email="aaron.childs@thesocialoutfitus.com",
+            cultivator=strain_dict[1],
+        )
+        flower_data["overall_score"] = strain_dict[2]
+        
+        return_strains.append(flower_data)
+
 
     return return_strains
