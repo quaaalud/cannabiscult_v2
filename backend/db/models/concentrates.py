7967e631f1d0c50806f63fc8a6f516ff8cba1b44
Dale <dludwins@outlook.com>
2023-12-14 21:19:19 -0500

    working on concentrates

--- backend/db/models/concentrates.py
+++ backend/db/models/concentrates.py
@@ -6,40 +6,49 @@ Created on Sun Nov  5 17:09:03 2023
 @author: dale
 """
 
-from sqlalchemy import Column, BigInteger, Text, Boolean, ForeignKey
+from sqlalchemy import Column, BigInteger, Text, Boolean, ForeignKey, CheckConstraint
 from sqlalchemy.dialects.postgresql import ARRAY
-from sqlalchemy.sql import func
 from db.base_class import Base
 
 
 class Concentrate(Base):
-    __table_args__ = {'schema': 'public'}
+    __table_args__ = {"schema": "public"}
 
-    cultivator = Column(
-        Text,
-        nullable=False
-    )
-    strain = Column(
-        Text,
-        nullable=False
-    )
-    card_path = Column(
-        Text,
-        nullable=True
-    )
+    cultivator = Column(Text, nullable=False)
+    strain = Column(Text, nullable=False)
+    card_path = Column(Text, nullable=True)
     voting_open = Column(
         Boolean,
         default=True,
         nullable=False,
     )
     concentrate_id = Column(
-        BigInteger,
-        primary_key=True,
-        index=True,
-        autoincrement="auto",
-        nullable=False
+        BigInteger, primary_key=True, index=True, autoincrement="auto", nullable=False
     )
     is_mystery = Column(
         Boolean,
         default=True,
+    )
+
+
+class Concentrate_Description(Base):
+    __table_args__ = {"schema": "public"}
+    description_id = Column(BigInteger, primary_key=True, autoincrement=True)
+    concentrate_id = Column(
+        BigInteger,
+        ForeignKey("public.concentrate.concentrate_id", onupdate="CASCADE"),
+        nullable=True,
+    )
+    description = Column(Text, nullable=False, default="Coming Soon", server_default="Coming Soon")
+    effects = Column(Text, nullable=False, default="Coming Soon", server_default="Coming Soon")
+    lineage = Column(Text, nullable=False, default="Coming Soon", server_default="Coming Soon")
+    terpenes_list = Column(ARRAY(Text), nullable=True)
+    cultivar_email = Column(Text, ForeignKey("mysteryvoter.email"), nullable=False)
+
+    __table_args__ = (
+        CheckConstraint(
+            "length(description) < 1500", name="concentrate_descriptions_description_check"
+        ),
+        CheckConstraint("length(effects) < 1500", name="concentrate_descriptions_effects_check"),
+        CheckConstraint("length(lineage) < 1500", name="concentrate_descriptions_lineage_check"),
     )
--- backend/db/repository/concentrates.py
+++ backend/db/repository/concentrates.py
@@ -3,62 +3,98 @@
 from pathlib import Path
 from sqlalchemy.orm import Session
 from typing import Optional, Any, Dict, List
-from db.models.concentrates import Concentrate
+from db.models.concentrates import Concentrate, Concentrate_Description
 from db._supabase.connect_to_storage import return_image_url_from_supa_storage
+import traceback
 
 
-def get_concentrate_data_and_path(
-        db: Session,
-        strain: str) -> Optional[Dict[str, Any]]:
-    concentrate = db.query(
-        Concentrate
-    ).filter(
-        (Concentrate.strain == strain)
-    ).first()
+def get_concentrate_data_and_path(db: Session, strain: str) -> Optional[Dict[str, Any]]:
+    concentrate = db.query(Concentrate).filter((Concentrate.strain == strain)).first()
     if concentrate:
         return {
-            'id': concentrate.concentrate_id,
-            'cultivator': concentrate.cultivator,
-            'strain': concentrate.strain,
-            'url_path': return_image_url_from_supa_storage(
-                str(Path(concentrate.card_path))
-            ),
-            'voting_open': concentrate.voting_open,
-            'is_mystery': concentrate.is_mystery,
+            "id": concentrate.concentrate_id,
+            "cultivator": concentrate.cultivator,
+            "strain": concentrate.strain,
+            "url_path": return_image_url_from_supa_storage(str(Path(concentrate.card_path))),
+            "voting_open": concentrate.voting_open,
+            "is_mystery": concentrate.is_mystery,
         }
     return None
 
 
 async def get_concentrate_by_strain_and_cultivator(
-        db: Session,
-        strain: str,
-        cultivator: str) -> Optional[Dict[str, Any]]:
-    concentrate = db.query(
-        Concentrate
-    ).filter(
-        (Concentrate.strain == strain),
-        (Concentrate.cultivator == cultivator)
-    ).first()
+    db: Session, strain: str, cultivator: str
+) -> Optional[Dict[str, Any]]:
+    concentrate = (
+        db.query(Concentrate)
+        .filter((Concentrate.strain == strain), (Concentrate.cultivator == cultivator))
+        .first()
+    )
     if concentrate:
         return {
-            'id': concentrate.concentrate_id,
-            'cultivator': concentrate.cultivator,
-            'strain': concentrate.strain,
-            'url_path': return_image_url_from_supa_storage(
-                str(Path(concentrate.card_path))
-            ),
-            'voting_open': concentrate.voting_open,
-            'is_mystery': concentrate.is_mystery,
+            "id": concentrate.concentrate_id,
+            "cultivator": concentrate.cultivator,
+            "strain": concentrate.strain,
+            "url_path": return_image_url_from_supa_storage(str(Path(concentrate.card_path))),
+            "voting_open": concentrate.voting_open,
+            "is_mystery": concentrate.is_mystery,
         }
     return None
 
 
 def get_vibe_concentrate_strains(db: Session) -> Optional[List[str]]:
-    concentrate_list = db.query(
-        Concentrate
-    ).filter(
-        (Concentrate.cultivator == 'Vibe')
-    ).all()
+    concentrate_list = db.query(Concentrate).filter((Concentrate.cultivator == "Vibe")).all()
     if concentrate_list:
         return [concentrate.strain for concentrate in concentrate_list]
     return None
+
+
+def get_concentrate_and_description(
+    db: Session,
+    strain: str,
+    cultivar_email: str = "aaron.childs@thesocialoutfitus.com",
+    cultivator: str = "",
+) -> Optional[Dict[str, Any]]:
+    try:
+        query = (
+            db.query(Concentrate, Concentrate_Description)
+            .join(
+                Concentrate_Description,
+                Concentrate.concentrate_id == Concentrate_Description.concentrate_id,
+            )
+            .filter(Concentrate_Description.cultivar_email == cultivar_email)
+        )
+        print(query)
+
+        if cultivator:
+            query = query.filter(Concentrate.cultivator == cultivator)
+
+        query = query.filter(Concentrate.strain == strain)
+        concentrate_data = query.first()
+
+        if concentrate_data:
+            concentrate, description = concentrate_data
+
+            concentrate_info = {
+                "concentrate_id": concentrate.concentrate_id,
+                "cultivator": concentrate.cultivator,
+                "strain": concentrate.strain,
+                "url_path": return_image_url_from_supa_storage(str(Path(concentrate.card_path))),
+                "voting_open": concentrate.voting_open,
+                "is_mystery": concentrate.is_mystery,
+                "description_id": description.description_id,
+                "description_text": description.description,
+                "effects": description.effects,
+                "lineage": description.lineage,
+                "terpenes_list": description.terpenes_list,
+                "cultivar": description.cultivar_email,
+            }
+
+            return concentrate_info
+
+        return None
+
+    except Exception as e:
+        traceback.print_exc()
+        print(f"Error fetching concentrate data and description: {e}")
+        return None
