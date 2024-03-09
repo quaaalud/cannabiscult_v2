bb644c12b654b4917a6e9b41cc930c79e4d2237f
Dale <dludwins@outlook.com>
2024-03-08 20:44:03 -0500

    update cards

--- backend/apis/version1/_supabase/route_images.py
+++ backend/apis/version1/_supabase/route_images.py
@@ -67,13 +67,14 @@ async def update_product_image_url(product_id: str, file_path: str, supabase: Cl
 
 
 async def upload_image_to_supabase(
+    product_type: str,
     product_id: str,
     file: UploadFile = File(...),
     supabase: Client = Depends(_return_supabase_private_client),
 ) -> Dict[str, str]:
     if not product_id:
         return
-
+    storage_str = "additional_product_images"
     image_data = ImageUpload(image=file)
     if not image_data.image:
         raise HTTPException(status_code=400, detail="Invalid image file")
@@ -82,21 +83,21 @@ async def upload_image_to_supabase(
     temp_file_path = save_temporary_image(file)
 
     # List all files in the specific product directory to determine the new file's index
-    dir_list = supabase.storage.from_("additional_product_images").list()
+    dir_list = supabase.storage.from_(storage_str).list()
     if "error" in dir_list:
         raise HTTPException(status_code=500, detail=dir_list["error"]["message"])
 
-    if len(dir_list) > 0 and str(product_id) in [product["name"] for product in dir_list]:
-        dir_list = supabase.storage.from_("additional_product_images").list(path=product_id)
+    if len(dir_list) > 0 and str(product_type) in [product["name"] for product in dir_list]:
+        dir_list = supabase.storage.from_(storage_str).list(path=product_id)
 
     # Determine the index for the new file
     file_index = len(dir_list)
     # Construct the file path
-    file_path = f"{product_id}/{file_index}_{file.filename}"
+    file_path = f"{product_type}/{product_id}/{file_index}_{file.filename}"
     try:
         with open(temp_file_path, "rb") as f:
             mime_type = magic.from_buffer(f.read(1024), mime=True)
-            upload_response = supabase.storage.from_("additional_product_images").upload(
+            upload_response = supabase.storage.from_(storage_str).upload(
                 file=f, path=file_path, file_options={"content-type": mime_type}
             )
         if "error" in upload_response.text:
@@ -109,8 +110,9 @@ async def upload_image_to_supabase(
         return return_message
 
 
-@router.post("/images/{product_id}/upload/")
+@router.post("/{prodcut_type}/{product_id}/upload/")
 async def upload_product_image(
+    product_type: str,
     product_id: str,
     file: UploadFile = File(...),
     supabase: Client = Depends(_return_supabase_private_client),
@@ -118,22 +120,14 @@ async def upload_product_image(
     return await upload_image_to_supabase(product_id, file, supabase)
 
 
-@router.get("/images/{product_id}/")
+@router.get("/{product_type}/{product_id}/")
 async def list_product_images(
-    product_id: str, supabase: Client = Depends(_return_supabase_private_client)
+    product_type: str, product_id: str, supabase: Client = Depends(_return_supabase_private_client)
 ):
     try:
-        response = (
-            supabase.table("product").select("image_urls").eq("id", product_id).single().execute()
-        )
-
-        # Check if the response has an error attribute
-        if hasattr(response, "error") and response.error:
-            raise HTTPException(status_code=404, detail="Product not found")
-
-        if hasattr(response, "data") and response.data:
-            return {"image_urls": response.data.get("image_urls", [])}
-        else:
-            raise HTTPException(status_code=404, detail="Product not found")
+        image_urls = supabase.storage.from_(
+            "additional_product_images"
+        ).list(f"{product_type}/{product_id}")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    return [img["name"] for img in image_urls]
