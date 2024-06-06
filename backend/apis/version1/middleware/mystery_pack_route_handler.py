652c879f9e068208b90c4db6874353d11f06753e
Dale <dludwins@outlook.com>
2024-06-05 21:38:17 -0500

    updated mystery handler

--- backend/apis/version1/middleware/mystery_pack_route_handler.py
+++ backend/apis/version1/middleware/mystery_pack_route_handler.py
@@ -27,7 +27,8 @@ class LegacyURLMiddleware(BaseHTTPMiddleware):
             "Cult Flower 3": ("Robust", "Government Oasis"),
             "Cult Flower 4": ("Local", "RS11"),
             "Cult Rosin 1": ("Vibe", "Liquid Sunshine"),
-            "Cult Rosin 2": ("Robust", "Orange Runtz"), "Cult Rosin 3": ("Local", "RS11")
+            "Cult Rosin 2": ("Robust", "Orange Runtz"),
+            "Cult Rosin 3": ("Local", "RS11"),
         }
         # Extract query parameters
         query_params = dict(request.query_params)
